07bf6607c269826c983bb2b2dde247615a3df268
Sanchit Sinha <sanchitsinha14@gmail.com>
2025-08-06 11:30:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,9 +5,8 @@
 import {genkit, Plugin} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import {next} from '@genkit-ai/next';
-import {firebase} from '@genkit-ai/firebase';
 
-const plugins: Plugin<any>[] = [next(), firebase(), googleAI()];
+const plugins: Plugin<any>[] = [next(), googleAI()];
 
 if (!process.env.GENKIT_ENV || process.env.GENKIT_ENV === 'development') {
   // In a development environment, we provide a dev-only plugin that provides
